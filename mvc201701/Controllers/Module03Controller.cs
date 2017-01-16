a1b1dbf5d302899e7fa4609b317bedc3ccb57f78
Michell Cronberg <michell@cronberg.dk>
2017-01-16 16:05:06 +0100

    Start module 03

---
+++ mvc201701/Controllers/Module03Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc201701.Controllers
+{
+    public class Module03Controller : Controller
+    {
+        // GET: Module03
+        public ActionResult Index()
+        {
+            var ph = new mvc201701.Models.Module03.PersonHelper();
+            var p = ph.GetPerson(1);
+            return View(p);
+        }
+
+        public ActionResult Index2()
+        {
+            var ph = new mvc201701.Models.Module03.PersonHelper();
+            var p = ph.GetPerson(1);
+            return View(p);
+        }
+    }
+}
---
+++ mvc201701/Models/Module03/PersonHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc201701.Models.Module03
+{
+    public class PersonHelper
+    {
+        public Person GetPerson(int id)
+        {
+            Person p = new Person() { Id = id, Name = "Test", BirthDate = DateTime.Now };
+            return p;
+        }
+    }
+}
