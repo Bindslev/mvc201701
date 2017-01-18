e2d2f3a2c35a7b65bae540315f2483e0331247b8
Michell Cronberg <michell@cronberg.dk>
2017-01-18 16:11:07 +0100

    ..

---
+++ mvc201701/Controllers/Module07Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc201701.Controllers
+{
+    public class Module07Controller : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetPersons()
+        {
+            var h = new mvc201701.Models.Module03.PersonHelper();
+            var l = h.GetPersons();
+            return Json(l, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
--- mvc201701/Models/Module03/PersonHelper.cs
+++ mvc201701/Models/Module03/PersonHelper.cs
@@ -12,6 +12,16 @@ namespace mvc201701.Models.Module03
             Person p = new Person() { Id = id, Gender= Gender.Male, Name = "Test", BirthDate = DateTime.Now };
             return p;
         }
+
+        public List<Person> GetPersons()
+        {
+            Person p1 = new Person() { Id = 1, Gender = Gender.Male, Name = Guid.NewGuid().ToString(), BirthDate = DateTime.Now };
+            Person p2 = new Person() { Id = 2, Gender = Gender.Male, Name = Guid.NewGuid().ToString(), BirthDate = DateTime.Now };
+            List<Person> lst = new List<Person>();
+            lst.Add(p1);
+            lst.Add(p2);
+            return lst;
+        }
         public Person GetPersonError(int id)
         {
             object p = null;
